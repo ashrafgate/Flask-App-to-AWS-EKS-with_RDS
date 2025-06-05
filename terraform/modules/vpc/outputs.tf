69150c7a944df23e849f800cb1ca209a51ab2b19
ashrafgate <66882690+ashrafgate@users.noreply.github.com>
2025-06-05 23:35:37 +0530

    Update outputs.tf

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -9,10 +9,7 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
   value = aws_subnet.private[*].id
 }
-output "eks_node_sg_id" {
-  description = "Security group ID for the EKS worker nodes"
-  value       = aws_security_group.eks_node_sg.id
-}
+
 output "rds_sg_id" {
   value = aws_security_group.rds_sg.id
 }
@@ -21,6 +18,4 @@ output "eks_node_sg_id" {
   value = aws_security_group.eks_node_sg.id
 }
 
-output "private_subnet_ids" {
-  value = aws_subnet.private[*].id
-}
+
