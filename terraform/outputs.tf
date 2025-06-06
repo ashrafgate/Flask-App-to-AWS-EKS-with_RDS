6110bafab2ca0fce9f8e1892cc487a00ba6d94c3
ashrafgate <66882690+ashrafgate@users.noreply.github.com>
2025-06-06 17:03:00 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,6 +3,10 @@ output "ecr_repository_url" {
   value       = module.ecr.repository_url
 }
 
-output "rds_endpoint" {
-  value = module.rds.db_endpoint
+#output "rds_endpoint" {
+#  value = module.rds.db_endpoint
+#}
+
+output "rds_hostname" {
+  value = aws_db_instance.main.address # returns only the hostname (no port)
 }
